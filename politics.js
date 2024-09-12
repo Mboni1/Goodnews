f481ee7cbcb4c279ca86e7a11bb8459db6658408
Mboni1 <mbonimpaclaude27@gmail.com>
2024-09-12 16:07:41 +0200

    change on index.html, index.js, politics.css, politics.html,politics.js, style.css

--- politics.js
+++ politics.js
@@ -1,6 +1,8 @@
-const menu=document.querySelector('#mobile-menu')
-const menuLinks=document.querySelector('.navbar-menu')
-menu.addEventListener('click', function() {
+const menu=document.querySelector('#mobile-menu');
+const menuLinks=document.querySelector('.navbar__menu');
+//Display Mobile Menu
+const mobileMenu = () => {
 menu.classList.toggle('is-active');
-menuLinks.classList.toggle('active')
-});
+menuLinks.classList.toggle('active');
+};
+menu.addEventListener('click',mobileMenu);
