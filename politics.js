2ce76be116b5be049bf5fa0944b13d3456b67caf
Mboni1 <mbonimpaclaude27@gmail.com>
2024-09-25 22:39:32 +0200

    add variables.html

--- politics.js
+++ politics.js
@@ -6,3 +6,29 @@ menu.classList.toggle('is-active');
 menuLinks.classList.toggle('active');
 };
 menu.addEventListener('click',mobileMenu);
+// Elements
+const signUpBtn = document.getElementById('signUpBtn');
+const signUpForm = document.getElementById('signUpForm');
+const signInForm = document.getElementById('signInForm');
+const showSignIn = document.getElementById('showSignIn');
+const showSignUp = document.getElementById('showSignUp');
+
+// Show Sign Up Form
+signUpBtn.addEventListener('click', () => {
+    signUpForm.classList.remove('hidden');
+    signInForm.classList.add('hidden');
+});
+
+// Switch to Sign In Form
+showSignIn.addEventListener('click', (e) => {
+    e.preventDefault();
+    signInForm.classList.remove('hidden');
+    signUpForm.classList.add('hidden');
+});
+
+// Switch to Sign Up Form
+showSignUp.addEventListener('click', (e) => {
+    e.preventDefault();
+    signUpForm.classList.remove('hidden');
+    signInForm.classList.add('hidden');
+});
